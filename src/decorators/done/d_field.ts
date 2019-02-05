bb4dcd7b2f52d9576bbc0feaf9a52a1f6b32a7b4
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2019-02-05 09:51:33 +0200

    reflect-metadata field example - Done!

--- src/decorators/done/d_field.ts
+++ src/decorators/done/d_field.ts
@@ -22,6 +22,8 @@ class FieldDecoratorEx2 {
 
   @format7("value4")
   s4: string;
+
+  s5: string;
 }
 
 let fd = new FieldDecoratorEx1();
@@ -32,3 +34,21 @@ console.log(Reflect.getMetadata(key_, fd, "s2"));
 // access via prototype
 console.log(Reflect.getMetadata(key_, FieldDecoratorEx2.prototype, "s3"));
 console.log(Reflect.getMetadata(key_, FieldDecoratorEx2.prototype, "s4"));
+
+console.log(Reflect.getMetadata(key_, FieldDecoratorEx2.prototype, "s5")); // undefined
+console.log(Reflect.hasMetadata(key_, FieldDecoratorEx2.prototype, "s5")); // false
+Reflect.set(FieldDecoratorEx2.prototype, "s5", "HELLO"); // set property value
+
+let fd2 = new FieldDecoratorEx2();
+console.log(fd2.s5);
+
+let fd3 = new FieldDecoratorEx2();
+console.log(fd3.s5);
+
+Reflect.defineProperty(FieldDecoratorEx2.prototype, "s6", {value: "property injected in runtime!"});
+
+let fd4 = new FieldDecoratorEx2();
+// @ts-ignore
+console.log(fd3.s6);
+// @ts-ignore
+console.log(fd2.s6);
