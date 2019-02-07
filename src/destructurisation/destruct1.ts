778e707ac5b0559c230cdf66209049ace6d1540f
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2019-02-07 15:31:07 +0200

    destruct

--- src/destructurisation/destruct1.ts
+++ src/destructurisation/destruct1.ts
@@ -1,14 +1,43 @@
-let vasya = {
-  id: 5,
-  name: 'Vasya',
-  active: true
-};
+namespace Destruct1 {
+  let vasya = {
+    id: 5,
+    name: 'Vasya',
+    active: true
+  };
 
-console.log(vasya);
-console.log({vasya});
-console.log({...vasya});
+  console.log(vasya);
+  console.log({vasya: vasya});
+  console.log({vasya});
+  console.log({...vasya});
 
-let arr = [1,2,3];
-console.log(arr);
-console.log({...arr});
+  let arr = [1, 2, 3];
+  console.log(arr);
+  console.log({...arr});
 
+  let a1 = ['a1', 'b1', 'c1'];
+  let a2 = ['a2', 'b2', 'c2'];
+  console.log(a1);
+  console.log([a1]);
+  console.log([a1, a2]);
+  console.log([...a1, ...a2]);
+
+  let obj = {
+    name: "Alex",
+    age: 21,
+    smart: 13
+  };
+
+  let {name, age} = obj;
+  console.log(obj.name);
+  console.log(name);
+  console.log(obj.age);
+  console.log(age);
+
+  const {"name": name1} = obj;
+  console.log(name1);
+
+  let {"name": n, ...rest} = obj;
+  console.log(n); // Alex
+  console.log(rest); // { age: 21, smart: 13 }
+
+}
---
+++ src/index3.ts
@@ -0,0 +1,6 @@
+namespace Test1 {
+  let s:string = 'abcdefghij';
+  let s1 = s.slice(1,5);
+  console.log(s1);
+
+}
