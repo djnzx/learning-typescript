c88f5581a5949b122ff01c09ef0fb7877e4e22a6
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2019-05-17 11:37:34 +0300

    functional #2

---
+++ src/fpts/scratch1.ts
@@ -0,0 +1,53 @@
+import { none, Option, some } from "fp-ts/lib/Option";
+import { Task } from "fp-ts/lib/Task";
+import { tryCatch } from "fp-ts/lib/TaskEither";
+
+const convert = (origin: string): Option<number> => {
+  const val = +origin;
+  return isNaN(val) || val % 1 !== 0 ? none : some(val);
+};
+
+console.log(convert('1'));
+console.log(convert('z1'));
+
+const decision42 = () => Promise.resolve(42);
+const decision43 = () => Promise.resolve(43);
+const printer = (x: number) => console.log(x);
+
+const x = decision42;   // function: x().then()
+const y = decision42(); // promise:  y.then(...);
+console.log(x);
+console.log(y);
+
+const task42 = new Task(decision42);
+const task43 = new Task(decision43);
+
+// always OK
+const deepThought = new Task<number>(() => Promise.resolve(42));
+
+deepThought.run().then(n => {
+  console.log(`The answer is ${n}.`)
+});
+
+interface Outcome {
+  name: string;
+}
+
+// can BREAK
+const fetchGreeting = (origin: string) => tryCatch<Error, Outcome>(
+  () => new Promise((resolve, reject) => resolve(JSON.parse(origin)) ),
+  reason => new Error(String(reason))
+);
+
+const source_data = [
+  '{ "name": "Carol" }',
+  '{ "name": "Carol }'
+];
+source_data.forEach(item =>
+  fetchGreeting(item).run()
+    .then(te => te.fold(
+      err => `Wrong: (${err.message})`,
+      x => `Right: ${x.name}!`
+    ))
+    .then(s => console.log(s))
+);
--- src/tsmonadx/sample1.ts
+++ src/tsmonadx/sample1.ts
@@ -1,6 +1,6 @@
 import { Either } from "tsmonad42";
 
-Either.left<string, number>('A')
+Either.right<string, number>(5)
   .lift(async v => Promise.resolve(v + 1))
   .lift(async v => Promise.resolve(await v + 1))
   .lift(async v => Promise.resolve(await v + 1))
