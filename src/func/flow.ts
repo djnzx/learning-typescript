9a4b4517adbb87cc38c2912e95d007a61f863124
alexr007 <alexey.rykhalskiy@gmail.com>
2019-05-20 22:55:16 +0300

    amazing syntax

--- src/func/flow.ts
+++ src/func/flow.ts
@@ -1,3 +1,16 @@
+import * as assert from 'assert';
+
+function pipe(...fns: Array<Function>): Function {
+  const len = fns.length - 1;
+  return function(this: any, x: any) {
+    let y = x;
+    for (let i = 0; i <= len; i++) {
+      y = fns[i].call(this, y)
+    }
+    return y
+  }
+}
+
 class Flow {
   constructor(readonly val: number) {}
 
@@ -51,3 +64,18 @@ const x = new Flow(1) // Flow
   .fold()                 // number
 
 console.log(x);
+
+const f_ = (n: number) => Promise.resolve(n + 1);
+const g_ = (n: number) => Promise.resolve(n * 2);
+// const h_ = (n: number) => Promise.resolve(n * 3);
+
+const promise_composition = async () =>
+  await pipe(
+    f_,
+    g_,
+    // h_,
+  )(2);
+// 1. f_: 2+1 = 3
+// 2. g_: 3*2 = 6
+
+promise_composition().then(console.log);
