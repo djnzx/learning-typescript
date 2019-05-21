75284118fb95609357a26c2a949d96921c0bf9a4
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2019-05-21 15:34:01 +0300

    functional ideas #9. incoming parameters typing #2

--- src/func/funcflow_playground_v2.ts
+++ src/func/funcflow_playground_v2.ts
@@ -1,8 +1,13 @@
 import { FuncFlow } from "./funcflow";
 
+type Tuple2 = [number, number]
+type Tuple3 = [number, number, number]
+type Tuple4 = [number, number, number, number]
+type Tuple5 = [number, number, number, number, number]
+
 FuncFlow.from(() => 2) // 2
-  .chain((x)                          => Promise.resolve([x, x + 1]) ) // [2, 3]
-  .chain(([x1, x2]: [number, number]) =>                       [x1, x2, x2 * 2]) // [2, 3, 6]
-  .chain(([x1, x2, x3])               => Promise.resolve([x1, x2, x3, x3 * 3]) ) // [2, 3, 6, 18]
-  .chain(([x1, x2, x3, x4])           =>                       [x1, x2, x3, x4, x4 + 1] ) // [2, 3, 6, 18, 19]
+  .chain((x):                    Promise<Tuple2> => Promise.resolve([x, x + 1]) ) // [2, 3]
+  .chain(([x1, x2]: Tuple2):             Tuple3  =>                       [x1, x2, x2 * 2]) // [2, 3, 6]
+  .chain(([x1, x2, x3]: Tuple3): Promise<Tuple4> => Promise.resolve([x1, x2, x3, x3 * 3]) ) // [2, 3, 6, 18]
+  .chain(([x1, x2, x3, x4]: Tuple4):     Tuple5  =>                       [x1, x2, x3, x4, x4 + 1] ) // [2, 3, 6, 18, 19]
   .then(console.log);
