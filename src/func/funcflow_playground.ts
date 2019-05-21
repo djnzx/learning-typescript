d4f298d0e9d5f03b6dc7b777a6284e836f15ac49
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2019-05-21 11:20:47 +0300

    functional ideas #7 - Done

--- src/func/funcflow_playground.ts
+++ src/func/funcflow_playground.ts
@@ -2,7 +2,7 @@ import { FuncFlow } from "./funcflow";
 
 FuncFlow.from(() => 2) // 2
   .chain( x => Promise.resolve(x + 1) ) // 3
-  .chain( x => Promise.resolve(x * 2) ) // 6
+  .chain( x => x * 2 ) // 6
   .chain( x => Promise.resolve(x * 3) ) // 18
-  .chain( x => Promise.resolve(x + 1) ) // 19
+  .chain( x => x + 1 ) // 19
   .then(console.log);
