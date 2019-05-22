e5f58f77afa6e8e4aefbffb0fab12d467ccff132
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2019-05-22 17:42:04 +0300

    interesting thoughts

--- src/func/funcflow_playground_v2.ts
+++ src/func/funcflow_playground_v2.ts
@@ -12,3 +12,8 @@ FuncFlow.from(() => 2) // 2
   .chain(([x1, x2, x3]: Tuple3): Promise<Tuple4> => Promise.resolve([x1, x2, x3, x3 * 3]) ) // [2, 3, 6, 18]
   .chain(([x1, x2, x3, x4]: Tuple4):     Tuple5  =>                       [x1, x2, x3, x4, x4 + 1] ) // [2, 3, 6, 18, 19]
   .then(console.log);
+
+FuncFlow.from(() => 2) // 2
+  .chain((x)        => Promise.resolve([x, x + 1]) ) // [2, 3]
+  .chain(([x1, x2]) =>                       [x1, x2, x2 * 2]) // [2, 3, 6]
+  .then(console.log);
