0aa13830b3281e345978d626ed3d29b0ec199145
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2019-05-22 18:23:42 +0300

    call/apply/bind

--- src/call_apply_bind/func2.ts
+++ src/call_apply_bind/func2.ts
@@ -9,18 +9,25 @@ const mix = {
     print: function() { return this.name; }
 };
 
+// standard behavior
 console.log(mix.print());
-console.log(mix.print.bind(alex)());
+
+// bind
+const binded = mix.print.bind(alex);
+console.log(binded());
+
+// call
 console.log(mix.print.call(alex));
 
-// apply
-
+// apply #1
 let a = [1,2,3];
 let b = [4,5];
 
-[].push.apply(a, b)
+[].push.apply(a, b);
 
-console.log(a)
+console.log(a);
 
+// apply #2
+// @ts-ignore
 console.log(Math.max(a));
 console.log(Math.max.apply(null, a));
