de792897c2a95b8be4ac6da20656ad42a6f4cd87
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2019-06-05 19:22:33 +0300

    rx @2

--- src/rx/ex3/observable3-many.ts
+++ src/rx/ex3/observable3-many.ts
@@ -1,20 +1,35 @@
-import { Observable } from "rxjs";
+import { Observable, PartialObserver, Subscriber } from "rxjs";
 
-const notifications3 = new Observable(observer => {
+const emitter = new Observable(observer => {
+  let counter = 0;
   const interval = setInterval(() => {
-    observer.next('New notification');
-  }, 2000);
+    if (counter<20) {
+      observer.next(counter++);
+    } else {
+      observer.complete();
+    }
+
+  }, 1000);
 
   return () => clearInterval(interval);
 });
 
-const subscription = notifications3.subscribe({
-  next: val => console.log(val)
-});
+const consumer1: PartialObserver<number> = {
+  next: value => { if (value < 10) console.log(`1st: ${value}`); },
+  // complete: () => { subscription1.unsubscribe(); }
+};
+const consumer2: PartialObserver<number> = {
+  next: value => { if (value > 10) console.log(`2nd: ${value}`); },
+  // complete: () => { subscription2.unsubscribe(); }
+};
+
+const subscription1 = emitter.subscribe(consumer1);
+const subscription2 = emitter.subscribe(consumer2);
 
 setTimeout(
-  () => subscription.unsubscribe(),
-  8000
+  // () => { subscription1.unsubscribe(); subscription2.unsubscribe(); },
+  () => {},
+  200000
 );
 
 console.log('Before');
