6371171186cf998032ab701a8c1f26be25cdf574
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2019-07-01 13:45:26 +0300

    null #2

--- src/null/nul001.ts
+++ src/null/nul001.ts
@@ -1,5 +1,13 @@
-const fp1 = async (): Promise<number | null> =>
+// GOOD
+const fp11 = async (): Promise<number | null> =>
   Promise.resolve(1);
 
-const fp2 = async (): Promise<number> | null =>
+const fp12 = async (): Promise<number | null> =>
+  Promise.resolve(1);
+
+// WRONG
+const fp21 = async (): Promise<number> | null =>
+  null;
+
+const fp22 = async (): Promise<number> | null =>
   null;
